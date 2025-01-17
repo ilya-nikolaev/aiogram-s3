7aa12169a22fb879b548f06a975aad97918e27a3
Ilya Nikolaev <65247719+ilya-nikolaev@users.noreply.github.com>
2025-01-17 12:50:49 +0300

    Updated example

--- examples/send_s3_doc.py
+++ examples/send_s3_doc.py
@@ -2,41 +2,71 @@
 import asyncio
 import logging
 import os
-from typing import TYPE_CHECKING
 
 from aioboto3 import Session
-from aiogram import Bot, Dispatcher, Router, types
+from aiogram import Bot, Dispatcher, F, Router, types
 from aiogram.filters import CommandStart
 
 from aiogram_s3 import S3Config
 from aiogram_s3.input_file import S3InputFile
 
-if TYPE_CHECKING:
-    from types_aiobotocore_s3 import S3Client
-
 router = Router()
+main_menu_keyboard = types.InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            types.InlineKeyboardButton(
+                text="By config",
+                callback_data="config",
+            ),
+        ],
+        [
+            types.InlineKeyboardButton(
+                text="By session",
+                callback_data="session",
+            ),
+        ],
+    ],
+)
 
 
 @router.message(CommandStart())
-async def process_photo(  # noqa: PLR0913
-    m: types.Message,
+async def process_photo(m: types.Message) -> None:
+    await m.answer("Hello!", reply_markup=main_menu_keyboard)
+
+
+@router.callback_query(F.data == "config")
+async def process_config_button(
+    cq: types.CallbackQuery,
+    bot: Bot,
     s3_config: S3Config,
-    s3_session: Session,
-    s3_client: "S3Client",
     s3_key: str,
     s3_bucket: str,
 ) -> None:
-    await m.answer_document(
+    if cq.message is None:
+        raise RuntimeError
+    await cq.answer()
+    await bot.send_document(
+        cq.message.chat.id,
         S3InputFile(s3_key, s3_bucket, s3_config=s3_config),
-        caption="Document got from S3 (config only)",
+        caption="Document got from S3 (by config)",
     )
-    await m.answer_document(
+
+
+@router.callback_query(F.data == "session")
+async def process_session_button(
+    cq: types.CallbackQuery,
+    bot: Bot,
+    s3_session: Session,
+    s3_key: str,
+    s3_bucket: str,
+) -> None:
+    if cq.message is None:
+        raise RuntimeError
+    await cq.answer()
+    await bot.send_document(
+        cq.message.chat.id,
         S3InputFile(s3_key, s3_bucket, s3_session=s3_session),
-        caption="Document got from S3 (session only)",
-    )
-    await m.answer_document(
-        S3InputFile(s3_key, s3_bucket, s3_client=s3_client),
-        caption="Document got from S3 (client only)",
+        caption="Document got from S3 (by session)",
     )
 
 
@@ -66,17 +96,8 @@ async def main() -> None:
         region_name=s3_config.region,
     )
 
-    s3_client = s3_session.client(
-        "s3",
-        region_name=s3_config.region,
-        endpoint_url=s3_config.endpoint_url,
-        aws_access_key_id=s3_config.aws_access_key_id,
-        aws_secret_access_key=s3_config.aws_secret_access_key,
-    )
-
     dp["s3_config"] = s3_config
     dp["s3_session"] = s3_session
-    dp["s3_client"] = s3_client
 
     dp["s3_key"] = get_required_env_variable("S3_KEY")
     dp["s3_bucket"] = get_required_env_variable("S3_BUCKET_NAME")
