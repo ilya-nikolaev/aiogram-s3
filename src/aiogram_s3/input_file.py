33738af45726b41e12917dbdeaf703bc4b6b9294
Ilya Nikolaev <65247719+ilya-nikolaev@users.noreply.github.com>
2025-01-25 15:01:31 +0300

    Removed the creation of dependencies

--- src/aiogram_s3/input_file.py
+++ src/aiogram_s3/input_file.py
@@ -1,74 +1,36 @@
-from collections.abc import AsyncGenerator
-from contextlib import asynccontextmanager
-from typing import TYPE_CHECKING
+from __future__ import annotations
 
-from aioboto3.session import Session
+from collections.abc import AsyncGenerator
+
+from aiobotocore.response import StreamingBody
 from aiogram import Bot
 from aiogram.types.input_file import DEFAULT_CHUNK_SIZE, InputFile
+from types_aiobotocore_s3 import S3Client
 
-from aiogram_s3.types import S3Bucket, S3Config, S3Key
-
-if TYPE_CHECKING:
-    from aiobotocore.response import StreamingBody
-    from types_aiobotocore_s3 import S3Client
+from aiogram_s3.types import S3Bucket, S3Key
 
 
 class S3InputFile(InputFile):
-    def __init__(  # noqa: PLR0913
+    def __init__(
         self,
         s3_key: S3Key,
         s3_bucket: S3Bucket,
-        s3_config: S3Config | None = None,
-        s3_session: Session | None = None,
+        s3_client: S3Client,
         filename: str | None = None,
         chunk_size: int = DEFAULT_CHUNK_SIZE,
     ) -> None:
         if filename is None:
             filename = s3_key
         super().__init__(filename, chunk_size)
-
-        self.s3_config = s3_config
-        self.s3_session = s3_session
-
         self.s3_key = s3_key
         self.s3_bucket = s3_bucket
-
-    def _get_session(self) -> Session:
-        if self.s3_session is not None:
-            return self.s3_session
-
-        if self.s3_config is not None:
-            return Session(
-                aws_access_key_id=self.s3_config.aws_access_key_id,
-                aws_secret_access_key=self.s3_config.aws_secret_access_key,
-                region_name=self.s3_config.region,
-            )
-
-        message = "Cannot create session without config"
-        raise ValueError(message)
-
-    @asynccontextmanager
-    async def _get_client(self) -> AsyncGenerator["S3Client", None]:
-        s3_session = self._get_session()
-        if self.s3_config is not None:
-            async with s3_session.client(
-                "s3",
-                region_name=self.s3_config.region,
-                endpoint_url=self.s3_config.endpoint_url,
-                aws_access_key_id=self.s3_config.aws_access_key_id,
-                aws_secret_access_key=self.s3_config.aws_secret_access_key,
-            ) as s3_client:
-                yield s3_client
-        else:
-            async with s3_session.client("s3") as s3_client:
-                yield s3_client
+        self.s3_client = s3_client
 
     async def read(self, bot: Bot) -> AsyncGenerator[bytes, None]:  # noqa: ARG002
-        async with self._get_client() as s3:
-            obj = await s3.get_object(
-                Bucket=self.s3_bucket,
-                Key=self.s3_key,
-            )
-            body: StreamingBody = obj["Body"]
-            async for chunk in body.iter_chunks(self.chunk_size):
-                yield chunk
+        obj = await self.s3_client.get_object(
+            Bucket=self.s3_bucket,
+            Key=self.s3_key,
+        )
+        body: StreamingBody = obj["Body"]
+        async for chunk in body.iter_chunks(self.chunk_size):
+            yield chunk
